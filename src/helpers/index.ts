baa1bacdad31e0f22f83df849b5fd2e06d291c50
Luís Cunha <luiscvnha@gmail.com>
2023-04-15 14:32:40 +0100

    add isClickOnScrollbar helper function

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -22,3 +22,33 @@ export function typeOf(value: unknown): string {
   if (isNullish(match) || isNullish(match[1])) { throw new TypeError(); }
   return match[1].toLowerCase();
 }
+
+
+export function isClickOnScrollbar(click: MouseEvent): boolean {
+  const target = click.target as HTMLElement;
+  const targetStyle = window.getComputedStyle(target);
+
+  // check if vertical scrollbar was clicked
+  const offsetLeft = click.offsetX;
+
+  const borderLeftWidth = Number.parseFloat(targetStyle.getPropertyValue("border-left-width")) || 0;
+  const borderRightWidth = Number.parseFloat(targetStyle.getPropertyValue("border-right-width")) || 0;
+
+  const verticalScrollbarLeft = target.clientWidth;
+  const verticalScrollbarRight = target.offsetWidth - borderLeftWidth - borderRightWidth;
+
+  if (offsetLeft >= verticalScrollbarLeft && offsetLeft < verticalScrollbarRight) {
+    return true;
+  }
+
+  // check if horizontal scrollbar was clicked
+  const offsetTop = click.offsetY;
+
+  const borderTopWidth = Number.parseFloat(targetStyle.getPropertyValue("border-top-width")) || 0;
+  const borderBottomWidth = Number.parseFloat(targetStyle.getPropertyValue("border-bottom-width")) || 0;
+
+  const horizontalScrollbarTop = target.clientHeight;
+  const horizontalScrollbarBottom = target.offsetHeight - borderTopWidth - borderBottomWidth;
+
+  return offsetTop >= horizontalScrollbarTop && offsetTop < horizontalScrollbarBottom;
+}
