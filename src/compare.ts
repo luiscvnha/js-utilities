2c613fdd4abfd72e0741566c60aba2d6930bcc45
Luís Cunha <luiscvnha@gmail.com>
2023-09-25 21:42:40 +0100

    turn Compare and PropertyRetriever into a frozen object

--- src/compare.ts
+++ src/compare.ts
@@ -1,11 +1,8 @@
 import { Comparer, Order } from "./common";
 
 
-export class Compare {
-  private constructor() {}
-
-
-  public static asStrings(order: Order = Order.Ascending, locales?: string | string[] | undefined, options?: Intl.CollatorOptions | undefined): Comparer<any> {
+export const Compare = Object.freeze({
+  asStrings(order: Order = Order.Ascending, locales?: string | string[] | undefined, options?: Intl.CollatorOptions | undefined): Comparer<any> {
     const collator = new Intl.Collator(locales, options);
 
     return order === Order.Ascending
@@ -15,9 +12,9 @@ export class Compare {
       : function(a: any, b: any): number {
         return collator.compare(String(b), String(a));
       };
-  }
+  },
 
-  public static strings(order: Order = Order.Ascending, locales?: string | string[] | undefined, options?: Intl.CollatorOptions | undefined): Comparer<string> {
+  strings(order: Order = Order.Ascending, locales?: string | string[] | undefined, options?: Intl.CollatorOptions | undefined): Comparer<string> {
     const collator = new Intl.Collator(locales, options);
 
     return order === Order.Ascending
@@ -25,9 +22,9 @@ export class Compare {
       : function(a: string, b: string): number {
         return collator.compare(b, a);
       };
-  }
+  },
 
-  public static numbers(order: Order = Order.Ascending): Comparer<number> {
+  numbers(order: Order = Order.Ascending): Comparer<number> {
     return order === Order.Ascending
       ? function(a: number, b: number): number {
         return a - b;
@@ -35,9 +32,9 @@ export class Compare {
       : function(a: number, b: number): number {
         return b - a;
       };
-  }
+  },
 
-  public static bigInts(order: Order = Order.Ascending): Comparer<bigint> {
+  bigInts(order: Order = Order.Ascending): Comparer<bigint> {
     return order === Order.Ascending
       ? function(a: bigint, b: bigint): number {
         return a > b ? 1 : (a < b ? -1 : 0);
@@ -45,9 +42,9 @@ export class Compare {
       : function(a: bigint, b: bigint): number {
         return b > a ? 1 : (b < a ? -1 : 0);
       };
-  }
+  },
 
-  public static dates(order: Order = Order.Ascending): Comparer<Date> {
+  dates(order: Order = Order.Ascending): Comparer<Date> {
     return order === Order.Ascending
       ? function(a: Date, b: Date): number {
         return a.valueOf() - b.valueOf();
@@ -56,4 +53,4 @@ export class Compare {
         return b.valueOf() - a.valueOf();
       };
   }
-}
+});
--- src/property-retriever.ts
+++ src/property-retriever.ts
@@ -1,87 +1,78 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Enumerability_and_ownership_of_properties#obtaining_properties_by_enumerabilityownership
-export class PropertyRetriever {
-  private constructor() {}
-
-
-  /* public */
-
-
-  public static getOwnEnumerable(obj: object): string[] {
+export const PropertyRetriever = Object.freeze({
+  getOwnEnumerable(obj: object): string[] {
     return Object.keys(obj);
-  }
+  },
 
-  public static getOwnNonEnumerable(obj: object): string[] {
-    return PropertyRetriever._getPropertyNames(obj, true, false, PropertyRetriever._nonEnumerable);
-  }
+  getOwnNonEnumerable(obj: object): string[] {
+    return _getPropertyNames(obj, true, false, _nonEnumerable);
+  },
 
-  public static getOwn(obj: object): string[] {
+  getOwn(obj: object): string[] {
     return Object.getOwnPropertyNames(obj);
-  }
+  },
 
-  public static getInheritedEnumerable(obj: object): string[] {
-    return PropertyRetriever._getPropertyNames(obj, false, true, PropertyRetriever._enumerable);
-  }
+  getInheritedEnumerable(obj: object): string[] {
+    return _getPropertyNames(obj, false, true, _enumerable);
+  },
 
-  public static getInheritedNonEnumerable(obj: object): string[] {
-    return PropertyRetriever._getPropertyNames(obj, false, true, PropertyRetriever._nonEnumerable);
-  }
+  getInheritedNonEnumerable(obj: object): string[] {
+    return _getPropertyNames(obj, false, true, _nonEnumerable);
+  },
 
-  public static getInherited(obj: object): string[] {
-    return PropertyRetriever._getPropertyNames(obj, false, true, PropertyRetriever._enumerableAndNonEnumerable);
-  }
+  getInherited(obj: object): string[] {
+    return _getPropertyNames(obj, false, true, _enumerableAndNonEnumerable);
+  },
 
-  public static getEnumerable(obj: object): string[] {
+  getEnumerable(obj: object): string[] {
     const props: string[] = [];
     for (const prop in obj) {
       props.push(prop);
     }
     return props;
+  },
+
+  getNonEnumerable(obj: object): string[] {
+    return _getPropertyNames(obj, true, true, _nonEnumerable);
+  },
+
+  getAll(obj: object): string[] {
+    return _getPropertyNames(obj, true, true, _enumerableAndNonEnumerable);
   }
-
-  public static getNonEnumerable(obj: object): string[] {
-    return PropertyRetriever._getPropertyNames(obj, true, true, PropertyRetriever._nonEnumerable);
-  }
-
-  public static getAll(obj: object): string[] {
-    return PropertyRetriever._getPropertyNames(obj, true, true, PropertyRetriever._enumerableAndNonEnumerable);
-  }
+});
 
 
-  /* private */
+function _enumerable(obj: object, prop: string): boolean {
+  return Object.prototype.propertyIsEnumerable.call(obj, prop);
+}
 
+function _nonEnumerable(obj: object, prop: string): boolean {
+  return !Object.prototype.propertyIsEnumerable.call(obj, prop);
+}
 
-  private static _enumerable(obj: object, prop: string): boolean {
-    return Object.prototype.propertyIsEnumerable.call(obj, prop);
-  }
+function _enumerableAndNonEnumerable(): boolean {
+  return true;
+}
 
-  private static _nonEnumerable(obj: object, prop: string): boolean {
-    return !Object.prototype.propertyIsEnumerable.call(obj, prop);
-  }
+function _getPropertyNames(obj: object, iterateSelf: boolean, iteratePrototype: boolean, includeProp: (obj: object, prop: string) => boolean): string[] {
+  const props: string[] = [];
 
-  private static _enumerableAndNonEnumerable(): boolean {
-    return true;
-  }
-
-  private static _getPropertyNames(obj: object, iterateSelf: boolean, iteratePrototype: boolean, includeProp: (obj: object, prop: string) => boolean): string[] {
-    const props: string[] = [];
-
-    do {
-      if (iterateSelf) {
-        for (const prop of Object.getOwnPropertyNames(obj)) {
-          if (!props.includes(prop) && includeProp(obj, prop)) {
-            props.push(prop);
-          }
+  do {
+    if (iterateSelf) {
+      for (const prop of Object.getOwnPropertyNames(obj)) {
+        if (!props.includes(prop) && includeProp(obj, prop)) {
+          props.push(prop);
         }
       }
+    }
 
-      if (!iteratePrototype) {
-        break;
-      }
+    if (!iteratePrototype) {
+      break;
+    }
 
-      iterateSelf = true;
-      obj = Object.getPrototypeOf(obj);
-    } while (obj);
+    iterateSelf = true;
+    obj = Object.getPrototypeOf(obj);
+  } while (obj);
 
-    return props;
-  }
+  return props;
 }
