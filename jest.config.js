9a25c1e9e2a74b79cd982d4857db1bbbc2bda6ed
Luís Cunha <luiscvnha@gmail.com>
2023-09-29 20:42:26 +0100

    update configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
-module.exports = {
-  transform: {"^.+\\.ts?$": "ts-jest"},
+/** @type {import('jest').Config} */
+const config = {
+  transform: {
+    "\\.ts$": "ts-jest"
+  },
   testEnvironment: "node",
-  testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  testRegex: "/tests/.+\\.test\\.ts$",
+  moduleFileExtensions: [
+    "ts", "js"
+  ]
 };
+
+module.exports = config;
