98c9c128192555bb4ac3a20aa90d26875e3738fb
Luís Cunha <luiscvnha@gmail.com>
2024-02-15 00:46:38 +0000

    Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export * from "./common";
-export * from "./property-retriever";
 export * from "./xpromises";
-export * from "./helpers"; // common
-export * from "./compare"; // common
-export * from "./lazy"; // common, helpers
+export * from "./helpers"; // common, property-retriever
+export * from "./property-retriever"; // helpers
+export * from "./lazy"; // common
+export * from "./compare"; // common, helpers
 export * from "./queue"; // common, helpers
 export * from "./stack"; // common, helpers
 export * from "./list"; // common, helpers, compare
