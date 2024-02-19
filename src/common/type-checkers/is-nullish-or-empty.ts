5630fc2ed0fdacf9a854d8f93b774181138dcf03
Luís Cunha <luiscvnha@gmail.com>
2024-02-19 02:38:19 +0000

    add overloads

--- src/common/type-checkers/is-nullish-or-empty.ts
+++ src/common/type-checkers/is-nullish-or-empty.ts
@@ -1,7 +1,9 @@
-import type { Nullish } from "../types/nullish";
+import type { Nullable } from "../types/nullable";
 import { isString } from "./is-string";
 
 
+export function isNullishOrEmpty(value: string): value is "";
+export function isNullishOrEmpty(value: unknown): value is Nullable<"">;
 export function isNullishOrEmpty(value: unknown): boolean {
   return value === null
     || value === undefined
--- src/common/type-checkers/is-nullish-or-whitespace.ts
+++ src/common/type-checkers/is-nullish-or-whitespace.ts
@@ -1,8 +1,10 @@
-import type { Nullish } from "../types/nullish";
+import type { Nullable } from "../types/nullable";
 import { isString } from "./is-string";
 
 
-export function isNullishOrWhitespace(value: unknown): value is Nullish | "" {
+export function isNullishOrWhitespace(value: string): boolean;
+export function isNullishOrWhitespace(value: unknown): value is Nullable<string>;
+export function isNullishOrWhitespace(value: unknown): boolean {
   return value === null
     || value === undefined
     || (isString(value) && value.trim().length <= 0);
