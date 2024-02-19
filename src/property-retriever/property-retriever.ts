9b6367aa7798b310fd2e458b9fabe6e1009a2d9f
Luís Cunha <luiscvnha@gmail.com>
2024-02-19 02:45:54 +0000

    add overloads

--- src/property-retriever/property-retriever.ts
+++ src/property-retriever/property-retriever.ts
@@ -40,36 +40,50 @@ function getPropertyNames(obj: object, iterateSelf: boolean, iteratePrototype: b
 }
 
 
+export function getOwnEnumerable<T>(obj: T): (keyof T & string)[];
+export function getOwnEnumerable(obj: object): string[];
 export function getOwnEnumerable(obj: object): string[] {
   return Object.keys(obj);
 }
 
 
+export function getOwnNonEnumerable<T>(obj: T): (keyof T & string)[];
+export function getOwnNonEnumerable(obj: object): string[];
 export function getOwnNonEnumerable(obj: object): string[] {
   return getPropertyNames(obj, true, false, nonEnumerable);
 }
 
 
+export function getOwn<T>(obj: T): (keyof T & string)[];
+export function getOwn(obj: object): string[];
 export function getOwn(obj: object): string[] {
   return Object.getOwnPropertyNames(obj);
 }
 
 
+export function getInheritedEnumerable<T>(obj: T): (keyof T & string)[];
+export function getInheritedEnumerable(obj: object): string[];
 export function getInheritedEnumerable(obj: object): string[] {
   return getPropertyNames(obj, false, true, enumerable);
 }
 
 
+export function getInheritedNonEnumerable<T>(obj: T): (keyof T & string)[];
+export function getInheritedNonEnumerable(obj: object): string[];
 export function getInheritedNonEnumerable(obj: object): string[] {
   return getPropertyNames(obj, false, true, nonEnumerable);
 }
 
 
+export function getInherited<T>(obj: T): (keyof T & string)[];
+export function getInherited(obj: object): string[];
 export function getInherited(obj: object): string[] {
   return getPropertyNames(obj, false, true, enumerableAndNonEnumerable);
 }
 
 
+export function getEnumerable<T>(obj: T): (keyof T & string)[];
+export function getEnumerable(obj: object): string[];
 export function getEnumerable(obj: object): string[] {
   const props: string[] = [];
   for (const prop in obj) {
@@ -79,11 +93,15 @@ export function getEnumerable(obj: object): string[] {
 }
 
 
+export function getNonEnumerable<T>(obj: T): (keyof T & string)[];
+export function getNonEnumerable(obj: object): string[];
 export function getNonEnumerable(obj: object): string[] {
   return getPropertyNames(obj, true, true, nonEnumerable);
 }
 
 
+export function getAll<T>(obj: T): (keyof T & string)[];
+export function getAll(obj: object): string[];
 export function getAll(obj: object): string[] {
   return getPropertyNames(obj, true, true, enumerableAndNonEnumerable);
 }
