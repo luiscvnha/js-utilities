0b2a79879156c7df8e25d7c92ff33591ab1110de
Luís Cunha <luiscvnha@gmail.com>
2024-03-24 19:58:38 +0000

    fix typing

--- src/common/is-nullish-or-whitespace.ts
+++ src/common/is-nullish-or-whitespace.ts
@@ -1,8 +1,7 @@
-import type { Nullable } from "./types/nullable";
 import { isString } from "./is-string";
 
 
-export function isNullishOrWhitespace(value: unknown): value is Nullable<string> {
+export function isNullishOrWhitespace(value: unknown): boolean {
   return value === null
     || value === undefined
     || (isString(value) && value.trim().length <= 0);
